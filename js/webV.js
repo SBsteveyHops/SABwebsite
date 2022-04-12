35ac0ec5deeec0a44ce6b7a6641980f51f75f787
SBsteveyHops <salvobonsma@gmail.com>
2022-04-12 11:08:54 -0700

    Cleanup, other...

---
+++ js/webV.js
@@ -0,0 +1,8 @@
+var websiteV = "0.49v";
+var betaV = "1.4.9";
+var StableV = "0.0";
+
+function version () {
+    document.getElementById('websiteV').innerHTML = 'A game made by: AshQumby and a_viper. ' + websiteV;
+    document.getElementById('betaV').innerHTML = 'Beta ' + betaV;
+}
